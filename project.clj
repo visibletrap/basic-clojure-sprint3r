649ccefd3f4caa79801c85aa1f06a44054be30e2
Tap <visibletrap@gmail.com>
2015-07-10 06:39:38 +0700

    Update ring to release version

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring/ring-core "1.4.0-RC2"]
-                 [ring/ring-jetty-adapter "1.4.0-RC2"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  [seesaw "1.4.5"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :plugins [[lein-ring "0.9.6"]]
